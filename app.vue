835cdf7954ec47984982ab24e887c3a825db7d0f
Juned Khatri <juned.khatri31@gmail.com>
2023-12-01 00:37:28 +0530

    Add SEO meta

--- app.vue
+++ app.vue
@@ -3,3 +3,18 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Cartoonify AI',
+    ogTitle: 'Cartoonify AI',
+    twitterTitle: 'Cartoonify AI',
+    description: 'Cartoonify AI: Transform any image into a vibrant 3D avatar effortlessly!',
+    ogDescription: 'Cartoonify AI: Transform any image into a vibrant 3D avatar effortlessly!',
+    twitterDescription: 'Cartoonify AI: Transform any image into a vibrant 3D avatar effortlessly!',
+    // ogImage: '/images/og-image-v1.png',
+    // twitterImage: '/images/og-image-v1.png',
+    // twitterCard: 'summary_large_image',
+})
+</script>
+
